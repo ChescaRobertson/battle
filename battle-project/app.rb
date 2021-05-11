2dc53a6ceb79e20aaf32f4984149f2d9305cb199
Francesca Robertson <58787784+ChescaRobertson@users.noreply.github.com>
2021-05-11 16:21:04 +0100

    Enter names complete and refactored

--- battle-project/app.rb
+++ battle-project/app.rb
@@ -10,5 +10,12 @@ class Battle < Sinatra::Base
     erb :index
   end
 
+  post '/names' do
+    @player_one_name = params[:player_one_name]
+    @player_two_name = params[:player_two_name]
+
+    erb :play
+  end
+
   run! if app_file == $0
 end 
