dfb5cad2c1b7f31f495d294b89ca66ce8ab5d3f4
Francesca Robertson <58787784+ChescaRobertson@users.noreply.github.com>
2021-05-13 11:19:19 +0100

    Variables updated

--- battle-project/app.rb
+++ battle-project/app.rb
@@ -19,20 +19,20 @@ class Battle < Sinatra::Base
   end
 
   get '/play' do
-    @player_one_name = $player_one.name
-    @player_two_name = $player_two.name
-    @player_one_hp = $player_one.hp
-    @player_two_hp = $player_two.hp
+    @player_one = $player_one
+    @player_two = $player_two
+
     erb :play
   end
 
   get '/attack' do
-    @player_one_name = $player_one.name
-    @player_two_name = $player_two.name
+    @player_one = $player_one
+    @player_two = $player_two
     $player_one.attack($player_two)
-    @player_one_hp = $player_one.hp
-    @player_two_hp = $player_two.hp
+  
     erb :play
+
+   
    end
 
   run! if app_file == $0
