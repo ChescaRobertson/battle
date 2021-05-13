5732d5218d9ee798d963ac6452acb2c168b5214b
Francesca Robertson <58787784+ChescaRobertson@users.noreply.github.com>
2021-05-13 16:16:16 +0100

    FINISHEDgit add .

--- battle-project/app.rb
+++ battle-project/app.rb
@@ -9,6 +9,10 @@ class Battle < Sinatra::Base
     register Sinatra::Reloader
   end
 
+  before do
+    @game = Game.instance
+  end
+
   get '/' do
     erb :index
   end
@@ -16,26 +20,22 @@ class Battle < Sinatra::Base
   post '/names' do
     player_1 = Player.new(params[:player_1_name])
     player_2 = Player.new(params[:player_2_name])
-    $game = Game.new(player_1, player_2)
+    @game = Game.create(player_1, player_2)
     redirect '/play'
   end
 
   get '/play' do
-    @game = $game
     erb :play
   end
 
   get '/attack' do
-    @game = $game
-    # @game.attack(@game.opponent_of(@game.current_turn))
     @game.switch_turn
     erb :play
    end
 
   post '/attack' do
-    @game = $game
     @game.attack(@game.opponent_of(@game.current_turn))
-    if $game.game_over?
+    if @game.game_over?
       redirect '/game_over'
     else
       redirect '/attack'
@@ -43,7 +43,6 @@ class Battle < Sinatra::Base
    end
 
    get '/game_over' do
-     @game = $game
      erb :game_over
    end
 
--- battle-project/lib/game.rb
+++ battle-project/lib/game.rb
@@ -9,6 +9,14 @@ class Game
     @current_turn = player_1
   end
 
+  def self.create(player_1, player_2)
+    @game = Game.new(player_1, player_2)
+  end
+
+  def self.instance
+    @game
+  end
+
   def player_1
     @players.first
   end
@@ -33,7 +41,7 @@ class Game
     losing_players.any?
   end
 
-  def loose
+  def loser
     losing_players.first
   end
 
